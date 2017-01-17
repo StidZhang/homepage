b4ab802edb37103dc878831ac48dadc80614aec4
Stid Zhang <stidzhang@gmail.com>
2017-01-17 18:39:34 -0500

    add pdf

--- client/config/router.js
+++ client/config/router.js
@@ -11,20 +11,13 @@ FlowRouter.route('/home', {
         DocHead.setTitle('Stid | Homepage');
     }
 
-});
-
-FlowRouter.route('/contact', {
-    name: "Contact Infomation",
-    action: function() {
-        BlazeLayout.render("mainLayout", {content: "contact"});
-        DocHead.setTitle('Stid | Contact');
-    }
-});
+})
 
 FlowRouter.route('/project', {
-    name: "Project Detail",
     action: function() {
         BlazeLayout.render("mainLayout", {content: "project"});
         DocHead.setTitle('Stid | Project');
     }
 });
+
+FlowRouter.route('/homepage.pdf')
