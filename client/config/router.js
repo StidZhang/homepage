d37933160145885d76c92715f5a36123043905bc
Stid Zhang <stidzhang@gmail.com>
2017-01-17 12:23:59 -0500

    init layout changes

--- client/config/router.js
+++ client/config/router.js
@@ -1,18 +1,30 @@
 
 FlowRouter.route('/', {
     action: function() {
-        FlowRouter.go('/pageOne');
+        FlowRouter.go('/home');
     }
 });
 
-FlowRouter.route('/pageOne', {
+FlowRouter.route('/home', {
     action: function() {
-        BlazeLayout.render("mainLayout", {content: "pageOne"});
+        BlazeLayout.render("mainLayout", {content: "home"});
+        DocHead.setTitle('Homepage | Stid Zhang');
+    }
+
+});
+
+FlowRouter.route('/contact', {
+    name: "Contact Infomation",
+    action: function() {
+        BlazeLayout.render("mainLayout", {content: "contact"});
+        DocHead.setTitle('Contact | Stid Zhang');
     }
 });
 
-FlowRouter.route('/pageTwo', {
+FlowRouter.route('/project', {
+    name: "Project Detail",
     action: function() {
-        BlazeLayout.render("mainLayout", {content: "pageTwo"});
+        BlazeLayout.render("mainLayout", {content: "project"});
+        DocHead.setTitle('Project | Stid Zhang');
     }
 });
